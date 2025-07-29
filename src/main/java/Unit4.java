8db3da687eb15e7ac445d6b2530dc5fc1c479cc4
Hoggspjot <vingisaar@gmail.com>
2025-07-29 23:37:14 +0300

    4 units added

--- src/main/java/Unit4.java
+++ src/main/java/Unit4.java
@@ -1,5 +1,17 @@
 public class Unit4 {
     public static void main(String[] args) {
 
+        int period1 = 20;
+        int period2 = 1440;
+        int period3 = 4320;
+        int period4 = 43200;
+
+        System.out.printf("За %d минут машина произвела %d штук бутылок\n",period1, period1*8);
+        System.out.printf("За %d минут машина произвела %d штук бутылок\n",period2, period2*8);
+        System.out.printf("За %d минут машина произвела %d штук бутылок\n",period3, period3*8);
+        System.out.printf("За %d минут машина произвела %d штук бутылок\n",period4, period4*8);
+
+
+
     }
 }
